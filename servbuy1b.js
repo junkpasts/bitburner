c9eb0923d0a215005d8d9b751fbe21ab13519246
junkpasts <junkpasts@gmail.com>
2025-08-12 19:09:12 +0300

    Create  servbuy1b.js

---
+++ servbuy1b.js
@@ -0,0 +1,25 @@
+/** @param {NS} ns */
+export async function main ( ns )
+{
+  const ram = 512;
+  let i = ns.getPurchasedServers ( ).length;
+  
+  while ( i < ns.getPurchasedServerLimit ( ) )
+  {
+    if ( ns.getServerMoneyAvailable ( "home" ) > ns.getPurchasedServerCost ( ram ) )
+    {
+      let hostname = ns.purchaseServer ( "pserv-" + i, ram );
+      if ( hostname !== "" )
+      {
+        ns.scp ( "early-hack-template.js", hostname );
+        ns.scp ( "bin.hk.js", hostname );
+        ns.scp ( "bin.gr.js", hostname );
+        ns.scp ( "bin.wk.js", hostname );
+        ns.exec ( "early-hack-template.js", hostname, 182 );
+      }
+      ++i;
+    }
+
+    await ns.sleep ( 1000 );
+  }
+}
